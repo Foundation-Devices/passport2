969830e4f8aba3c0fc098599bd9ebe7d5e2825be
eupn <eupn@protonmail.com>
2022-07-22 19:43:22 +0700

    Make file picker responsive to SD card changes

--- ports/stm32/boards/Passport/modules/flows/file_picker_flow.py
+++ ports/stm32/boards/Passport/modules/flows/file_picker_flow.py
@@ -12,9 +12,16 @@ from styles.colors import COPPER
 import microns
 import common
 from utils import get_file_list
+from uasyncio import sleep_ms
 
 
 class FilePickerFlow(Flow):
+    # Used to back up a previous SD card callback if any
+    prev_sd_card_cb = None
+
+    # SD card interrupt event
+    sd_card_change = False
+
     def __init__(
             self, initial_path=None, show_folders=False, enable_parent_nav=False, suffix=None,
             filter_fn=None):
@@ -27,77 +34,146 @@ class FilePickerFlow(Flow):
         self.filter_fn = filter_fn
 
     async def show_file_picker(self):
-        # Get list of files/folders at the current path
-        try:
-            active_idx = len(self.paths) - 1
-            active_path = self.paths[active_idx]
-            files = get_file_list(
-                active_path,
-                include_folders=self.show_folders,
-                include_parent=self.enable_parent_nav,
-                suffix=self.suffix,
-                filter_fn=self.filter_fn)
+        # Activate SD card hook
+        self.prev_sd_card_cb = CardSlot.get_sd_card_change_cb()
+        CardSlot.set_sd_card_change_cb(self.handle_sd_card_cb)
 
-            def file_key(f):
-                (filename, _fullpath, is_folder) = f
-                return '{}::{}'.format('0' if is_folder else '1', filename.lower())
+        while True:
+            # Get list of files/folders at the current path
+            try:
+                active_idx = len(self.paths) - 1
+                active_path = self.paths[active_idx]
+                files = get_file_list(
+                    active_path,
+                    include_folders=self.show_folders,
+                    include_parent=self.enable_parent_nav,
+                    suffix=self.suffix,
+                    filter_fn=self.filter_fn)
 
-            files = sorted(files, key=file_key)
+                def file_key(f):
+                    (filename, _fullpath, is_folder) = f
+                    return '{}::{}'.format('0' if is_folder else '1', filename.lower())
 
-        except CardMissingError:
-            self.goto(self.show_insert_microsd_error)
-            return
+                files = sorted(files, key=file_key)
 
-        is_root = active_path == CardSlot.get_sd_root()
-        if is_root:
-            title = 'microSD'
-            icon = lv.ICON_MICROSD
-        else:
-            leaf_folder_name = active_path.split('/')[-1]
-            title = leaf_folder_name
-            icon = lv.ICON_FOLDER
-
-        if len(files) == 0:
-            result = await StatusPage(
-                text='No files found',
-                card_header={'title': title, 'icon': icon},
-                icon=lv.LARGE_ICON_ERROR,
-                icon_color=COPPER,
-                left_micron=microns.Back,
-                right_micron=microns.Retry
-            ).show()
-            if not result:
-                self.set_result(None)
+            except CardMissingError:
+                self.goto(self.show_insert_microsd_error)
                 return
+
+            is_root = active_path == CardSlot.get_sd_root()
+            if is_root:
+                title = 'microSD'
+                icon = lv.ICON_MICROSD
             else:
-                # Refresh
-                return
-        else:
-            result = await FilePickerPage(
-                files=files,
-                card_header={'title': title, 'icon': icon}
+                leaf_folder_name = active_path.split('/')[-1]
+                title = leaf_folder_name
+                icon = lv.ICON_FOLDER
 
-            ).show()
+            if len(files) == 0:
+                status_page = StatusPage(
+                    text='No files found',
+                    card_header={'title': title, 'icon': icon},
+                    icon=lv.LARGE_ICON_ERROR,
+                    icon_color=COPPER,
+                    left_micron=microns.Back,
+                    right_micron=microns.Retry
+                )
+                status_page.display()
 
-        # No file selected - go back to previous page
-        if result is None:
-            common.page_transition_dir = TRANSITION_DIR_POP
-            if len(self.paths) <= 1:
-                self.set_result(None)
+                g = status_page.poll_for_done()
+                while True:
+                    try:
+                        next(g)
+                        await sleep_ms(10)
+
+                        # SD card just got inserted or removed
+                        if self.sd_card_change:
+                            self.sd_card_change = False
+
+                            try:
+                                with CardSlot():
+                                    return  # Refresh
+                            except CardMissingError:
+                                status_page.set_result(None)
+                                self.restore_sd_cb()
+                                self.goto(self.show_insert_microsd_error)
+                                return
+
+                    except StopIteration as result:
+                        result = result.value
+
+                        if not result:
+                            self.set_result(None)
+                            self.restore_sd_cb()
+                            return
+                        else:
+                            self.restore_sd_cb()
+                            return  # Refresh
+
+                    except Exception as e:
+                        self.restore_sd_cb()
+                        self.handle_fatal_error(e)
             else:
-                # Go back up a level
-                self.paths.pop(-1)
-            return
+                file_picker_page = FilePickerPage(
+                    files=files,
+                    card_header={'title': title, 'icon': icon}
+                )
+                file_picker_page.display()
 
-        _filename, full_path, is_folder = result
-        if is_folder:
-            common.page_transition_dir = TRANSITION_DIR_PUSH
-            self.paths.append(full_path)
-            return
+                g = file_picker_page.poll_for_done()
+                while True:
+                    try:
+                        next(g)
+                        await sleep_ms(10)
 
-        # User chose this file
-        common.page_transition_dir = TRANSITION_DIR_POP
-        self.set_result(result)
+                        # SD card just got inserted or removed
+                        if self.sd_card_change:
+                            self.sd_card_change = False
+
+                            try:
+                                with CardSlot():
+                                    pass
+                            except CardMissingError:
+                                file_picker_page.set_result(None)
+                                self.restore_sd_cb()
+                                self.goto(self.show_insert_microsd_error)
+                                return
+
+                    except StopIteration as result:
+                        result = result.value
+
+                        # No file selected - go back to previous page
+                        if result is None:
+                            common.page_transition_dir = TRANSITION_DIR_POP
+                            if len(self.paths) <= 1:
+                                self.set_result(None)
+                            else:
+                                # Go back up a level
+                                self.paths.pop(-1)
+
+                            self.restore_sd_cb()
+                            return
+
+                        _filename, full_path, is_folder = result
+                        if is_folder:
+                            common.page_transition_dir = TRANSITION_DIR_PUSH
+                            self.paths.append(full_path)
+                            file_picker_page.restore_statusbar_and_card_header()
+                            self.restore_sd_cb()
+
+                            return
+
+                        # User chose this file
+                        common.page_transition_dir = TRANSITION_DIR_POP
+                        self.set_result(result)
+
+                        file_picker_page.restore_statusbar_and_card_header()
+                        self.restore_sd_cb()
+
+                        return
+                    except Exception as e:
+                        self.restore_sd_cb()
+                        self.handle_fatal_error(e)
 
     async def show_insert_microsd_error(self):
         result = await InsertMicroSDPage().show()
@@ -105,3 +181,12 @@ class FilePickerFlow(Flow):
             self.set_result(None)
         else:
             self.goto(self.show_file_picker)
+
+    def handle_sd_card_cb(self):
+        if self.sd_card_change:
+            return
+
+        self.sd_card_change = True
+
+    def restore_sd_cb(self):
+        CardSlot.set_sd_card_change_cb(self.prev_sd_card_cb)
