f6deff9f53bf222580f7b80097818827e1073152
Ken Carpenter <ken@foundationdevices.com>
2023-01-04 23:07:46 -0800

    PASS1-576: Render camera image direct to LCD
    Instead of letting LVGL render pixel-by-pixel from the camera image downsampled to 1 bit-per-pixel, we now render directly to the LCD driver buffer and update just the modified lines.
    
    In addition, the LVGL `_lcd_rounder` callback was always setting the fully display to be updated (in addition to setting `direct_mode=1`, which ended up doing the same thing).  Change it so that only the dirty lines are push to the display.  Also optimized the bit calculation of `_lcd_set_px()` to be a table lookup instead for a small LVGL performance boost.

--- ports/stm32/boards/Passport/modpassport_lv-lcd.h
+++ ports/stm32/boards/Passport/modpassport_lv-lcd.h
@@ -20,8 +20,29 @@ STATIC mp_obj_t mod_passport_lv_lcd_init(void) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_passport_lv_lcd_init_obj, mod_passport_lv_lcd_init);
 
+#ifdef SCREEN_MODE_MONO
+STATIC mp_obj_t mod_passport_lv_lcd_update_viewfinder_direct(size_t n_args, const mp_obj_t* args) {
+    mp_buffer_info_t grayscale_info;
+    mp_get_buffer_raise(args[0], &grayscale_info, MP_BUFFER_READ);
+
+    mp_int_t hor_res = mp_obj_get_int(args[1]);
+    mp_int_t ver_res = mp_obj_get_int(args[2]);
+
+    lcd_update_viewfinder(grayscale_info.buf, hor_res, ver_res);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_passport_lv_lcd_update_viewfinder_direct_obj,
+                                           3,
+                                           3,
+                                           mod_passport_lv_lcd_update_viewfinder_direct);
+#endif
+
 STATIC const mp_rom_map_elem_t mod_passport_lv_lcd_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&mod_passport_lv_lcd_init_obj)},
+#ifdef SCREEN_MODE_MONO
+    {MP_ROM_QSTR(MP_QSTR_update_viewfinder_direct), MP_ROM_PTR(&mod_passport_lv_lcd_update_viewfinder_direct_obj)},
+#endif
 };
 STATIC MP_DEFINE_CONST_DICT(mod_passport_lv_lcd_globals, mod_passport_lv_lcd_globals_table);
 
