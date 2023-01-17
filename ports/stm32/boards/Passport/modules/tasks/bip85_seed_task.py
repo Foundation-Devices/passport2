65c6005f15626a8ce6e06b5dce84ca9cb80be831
Matt Gleason <mjgleason@foundationdevices.com>
2023-01-17 01:09:32 -0600

    PASS1-24: matched BIP 85 reference implementation

--- ports/stm32/boards/Passport/modules/tasks/bip85_seed_task.py
+++ ports/stm32/boards/Passport/modules/tasks/bip85_seed_task.py
@@ -4,12 +4,12 @@
 # bip85_seed_task.py - Task to create a new seed using BIP 85
 
 async def bip85_seed_task(on_done, index):
-    from serializations import sha512
+    from trezorcrypto import hmac
     import stash
-    path = "m/83696968'/39'/0'/24'/{}'".format(index)
 
+    path = "m/83696968'/39'/0'/24'/{}'".format(index)
     with stash.SensitiveValues() as sv:
         node = sv.derive_path(path)
-        entropy = sha512(node.private_key())
+        entropy = hmac(512, 'bip-entropy-from-k', node.private_key()).digest()
         seed = entropy[0:32]
     await on_done(seed, None)
