2953ffd679be27e1fc1f05067b3622cc731c6cdf
Matt Gleason <mjgleason@foundationdevices.com>
2023-01-20 11:46:00 -0600

    PASS1-24: added number of words selection

--- ports/stm32/boards/Passport/modules/tasks/bip85_seed_task.py
+++ ports/stm32/boards/Passport/modules/tasks/bip85_seed_task.py
@@ -3,13 +3,14 @@
 #
 # bip85_seed_task.py - Task to create a new seed using BIP 85
 
-async def bip85_seed_task(on_done, index):
+async def bip85_seed_task(on_done, num_words, index):
     from trezorcrypto import hmac
     import stash
 
-    path = "m/83696968'/39'/0'/24'/{}'".format(index)
+    path = "m/83696968'/39'/0'/{}'/{}'".format(num_words, index)
+    width = (num_words - 1) * 11 // 8 + 1
     with stash.SensitiveValues() as sv:
         node = sv.derive_path(path)
         entropy = hmac(512, 'bip-entropy-from-k', node.private_key()).digest()
-        seed = entropy[0:32]
+        seed = entropy[0:width]
     await on_done(seed, None)
