37ef7fc7b73c7031181039ec6b152c0f9de6fa92
Matt Gleason <mjgleason@foundationdevices.com>
2023-02-09 12:24:46 -0600

    PASS1-585: moved extension menu creation into extensions.py

--- ports/stm32/boards/Passport/modules/extensions/extensions.py
+++ ports/stm32/boards/Passport/modules/extensions/extensions.py
@@ -1,15 +1,20 @@
-# SPDX-FileCopyrightText: 2021 Foundation Devices, Inc. <hello@foundationdevices.com>
+# SPDX-FileCopyrightText: 2023 Foundation Devices, Inc. <hello@foundationdevices.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# sw_wallets.py - Software wallet config data for all supported wallets
+# extensions.py - Config data for all supported extensions
 #
 
 from .casa_extension import CasaExtension
 from .postmix_extension import PostmixExtension
 
-# Array of all supported software wallets and their attributes.
-# Used to build wallet menus and drive their behavior.
+# Array of all supported extensions.
+# Used to build extension menus and UI cards.
 supported_extensions = [
     CasaExtension,
     PostmixExtension,
+]
+
+supported_extensions_menu = [
+    CasaExtension['menu_item'],
+    PostmixExtension['menu_item'],
 ]
