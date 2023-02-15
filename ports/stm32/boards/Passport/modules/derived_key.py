7f5c38a1c62cab8166aea7982236159af6cd1d66
Matt Gleason <mjgleason@foundationdevices.com>
2023-02-15 15:30:34 -0600

    PASS1-24: completed passphrase support for the key manager, and improved Nostr key handling

--- ports/stm32/boards/Passport/modules/derived_key.py
+++ ports/stm32/boards/Passport/modules/derived_key.py
@@ -7,9 +7,9 @@ import lvgl as lv
 from tasks import bip85_seed_task, nostr_key_task
 
 key_types = {
-    'BIP85': {'icon': lv.ICON_BITCOIN, 'task': bip85_seed_task},
-    'Nostr': {'icon': lv.ICON_ONE_KEY, 'task': nostr_key_task},
+    'Bitcoin Child Seed': {'icon': lv.ICON_BITCOIN, 'task': bip85_seed_task},
+    'Nostr Key': {'icon': lv.ICON_ONE_KEY, 'task': nostr_key_task},
 }
 
 # This is unused, but it's an example of the schema
-# DEFAULT_DERIVED_KEY = {'name': 'First', 'index': 0, 'passphrase': False, 'type': 'BIP85'}
+# DEFAULT_DERIVED_KEY = {'name': 'First', 'index': 0, 'type': 'BIP85', 'xfp': <binary xfp>}
