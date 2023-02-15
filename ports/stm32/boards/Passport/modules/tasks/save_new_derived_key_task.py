2289e4a2a709d762933eb0f7920a0b5c57161a0d
Matt Gleason <mjgleason@foundationdevices.com>
2023-02-15 15:30:34 -0600

    PASS1-24: completed passphrase support for the key manager, and improved Nostr key handling

--- ports/stm32/boards/Passport/modules/tasks/save_new_derived_key_task.py
+++ ports/stm32/boards/Passport/modules/tasks/save_new_derived_key_task.py
@@ -4,14 +4,12 @@
 # save_new_derived_key_task.py - Task to save a new derived key
 
 
-async def save_new_derived_key_task(on_done, index, key_name, key_type):
+async def save_new_derived_key_task(on_done, index, key_name, key_type, xfp):
     from common import settings
-    import stash
     from utils import get_derived_keys
 
     keys = get_derived_keys()
-    has_passphrase = True if len(stash.bip39_passphrase) > 0 else False
-    keys.append({'name': key_name, 'index': index, 'passphrase': has_passphrase, 'type': key_type})
+    keys.append({'name': key_name, 'index': index, 'type': key_type, 'xfp': xfp})
     settings.set('derived_keys', keys)
     settings.save()
     # TODO: This can fail, so may need a try/except handler here
