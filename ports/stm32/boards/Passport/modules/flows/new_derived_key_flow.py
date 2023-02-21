1ca1011d533b2fe2bfc293f179f0183d5df0e471
Matt Gleason <mjgleason@foundationdevices.com>
2023-02-21 11:14:07 -0600

    PASS1-24: returned to key manager top level after creating a new key

--- ports/stm32/boards/Passport/modules/flows/new_derived_key_flow.py
+++ ports/stm32/boards/Passport/modules/flows/new_derived_key_flow.py
@@ -113,6 +113,8 @@ in the future. Do you want to continue?'''
         from pages import SuccessPage, ErrorPage
         from tasks import save_new_derived_key_task
         from utils import spinner_task
+        from common import keypad
+        import lvgl as lv
         (error,) = await spinner_task('Saving New Key Details', save_new_derived_key_task,
                                       args=[self.index,
                                             self.key_name,
@@ -127,38 +129,4 @@ in the future. Do you want to continue?'''
         else:
             await ErrorPage(text='New Seed Details not saved: {}'.format(error)).show()
             self.set_result(False)
-
-    async def choose_num_words(self):
-        from pages import ChooserPage
-        options = [{'label': '24 words', 'value': 24},
-                   {'label': '12 words', 'value': 12}]
-        self.num_words = await ChooserPage(card_header={'title': 'Number of Words'}, options=options).show()
-        if self.num_words is None:
-            self.set_result(False)
-            return
-        self.goto(self.generate_key)
-
-    async def generate_key(self):
-        from utils import spinner_task
-        from tasks import bip85_seed_task
-        from pages import ErrorPage
-        (self.seed, error) = await spinner_task(text='Generating Seed',
-                                                     task=bip85_seed_task,
-                                                     args=[self.num_words, self.index])
-        if self.error is None:
-            self.goto(self.show_seed_words)
-        else:
-            await ErrorPage(error).show()
-            self.set_result(False)
-
-    async def show_seed_words(self):
-        from flows import ViewSeedWordsFlow
-        await ViewSeedWordsFlow(bip85_seed=self.seed, bip85_index=self.index).run()
-        self.goto(self.show_qr_code)
-
-    async def show_qr_code(self):
-        from pages import ShowQRPage
-        from utils import B2A
-        import microns
-        await ShowQRPage(qr_data=B2A(self.seed), right_micron=microns.Checkmark).show()
-        self.set_result(True)
+        keypad.inject(lv.KEY.ESC)
