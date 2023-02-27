0989d1afa43327a93d5604560cc16dddf450448d
Matt Gleason <mjgleason@foundationdevices.com>
2023-02-27 12:13:22 -0600

    PASS1-24: finalized nostr key export code against referrence implementation

--- extmod/foundation/modtcc-codecs.c
+++ extmod/foundation/modtcc-codecs.c
@@ -133,10 +133,130 @@ STATIC mp_obj_t modtcc_bech32_encode(size_t n_args, const mp_obj_t *args) {
 }
 MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modtcc_bech32_encode_obj, 3, 4, modtcc_bech32_encode);
 
+STATIC mp_obj_t modtcc_bech32_plain_encode(size_t n_args, const mp_obj_t *args) {
+    const char*    hrp            = mp_obj_str_get_str(args[0]);
+    uint32_t       bech32_version = BECH32_ENCODING_BECH32;
+
+    if (n_args == 3) {
+        bech32_version = mp_obj_int_get_checked(args[2]);
+    }
+
+    mp_buffer_info_t buf;
+    mp_get_buffer_raise(args[1], &buf, MP_BUFFER_READ);
+
+
+    // low-level bech32 functions want 5-bit data unpacked into bytes. first value is
+    // the version number (5 bits), and remainder is packed data.
+
+    uint8_t data[buf.len];
+    size_t  data_len = 0;
+    int cv_ok        = sw_convert_bits(data, &data_len, 5, buf.buf, buf.len, 8, true);
+
+    if (cv_ok != 1) {
+        mp_raise_ValueError(MP_ERROR_TEXT("pack fail"));
+    }
+
+    vstr_t vstr;
+    vstr_init_len(&vstr, strlen(hrp) + data_len + 8);
+
+    /** Encode a Bech32 string
+     *
+     *  Out: output:  Pointer to a buffer of size strlen(hrp) + data_len + 8 that
+     *                will be updated to contain the null-terminated Bech32 string.
+     *  In: hrp :     Pointer to the null-terminated human readable part.
+     *      data :    Pointer to an array of 5-bit values.
+     *      data_len: Length of the data array.
+     *  Returns 1 if successful.
+
+        int bech32_encode(
+            char *output,
+            const char *hrp,
+            const uint8_t *data,
+            size_t data_len,
+            bech32_encoding enc
+        );
+     */
+    // printf("hrp=%s, data_len=%d\n", hrp, data_len);
+    int rv = bech32_encode(vstr.buf, hrp, data, data_len, bech32_version);
+
+    if (rv != 1) {
+        mp_raise_ValueError(MP_ERROR_TEXT("encode fail"));
+    }
+
+    vstr.len = strlen(vstr.buf);
+
+    return mp_obj_new_str_from_vstr(&mp_type_str, &vstr);
+}
+MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modtcc_bech32_plain_encode_obj, 2, 3, modtcc_bech32_plain_encode);
+
+STATIC mp_obj_t modtcc_bech32_decode(mp_obj_t enc) {
+    const char* s = mp_obj_str_get_str(enc);
+
+    /** Decode a Bech32 string
+ *
+ *  Out: hrp:      Pointer to a buffer of size strlen(input) - 6. Will be
+ *                 updated to contain the null-terminated human readable part.
+ *       data:     Pointer to a buffer of size strlen(input) - 8 that will
+ *                 hold the encoded 5-bit data values.
+ *       data_len: Pointer to a size_t that will be updated to be the number
+ *                 of entries in data.
+ *  In: input:     Pointer to a null-terminated Bech32 string.
+ *  Returns 1 if succesful.
+int bech32_decode(
+    char *hrp,
+    uint8_t *data,
+    size_t *data_len,
+    const char *input
+);
+ */
+
+    char    hrp[strlen(s) + 16];
+    uint8_t tmp[strlen(s) + 16];  // actually 8-bit
+    size_t  tmp_len = 0;
+
+    int rv = bech32_decode(hrp, tmp, &tmp_len, s);
+
+    if (rv != 1) {
+        // probably transcription error from user
+        mp_raise_ValueError(MP_ERROR_TEXT("corrupt bech32"));
+    }
+
+    if (tmp_len <= 1) {
+        // lots of valid Bech32 strings, but invalid for segwit puposes
+        // can end up here; but don't care.
+        mp_raise_ValueError(MP_ERROR_TEXT("no sw verion and/or data"));
+    }
+
+    // re-pack 5-bit data into 8-bit bytes (after version)
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wvla-larger-than="
+    uint8_t packed[tmp_len];
+    #pragma GCC diagnostic pop
+    size_t  packed_len = 0;
+
+    int cv_ok = sw_convert_bits(packed, &packed_len, 8, tmp + 1, tmp_len - 1, 5, false);
+
+    if (cv_ok != 1) {
+        mp_raise_ValueError(MP_ERROR_TEXT("repack fail"));
+    }
+
+    // return a tuple: (hrp, version, data)
+    mp_obj_t tuple[3] = {
+        mp_obj_new_str(hrp, strlen(hrp)),
+        MP_OBJ_NEW_SMALL_INT(tmp[0]),
+        mp_obj_new_bytes(packed, packed_len),
+    };
+
+    return mp_obj_new_tuple(3, tuple);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(modtcc_bech32_decode_obj, modtcc_bech32_decode);
+
 STATIC const mp_rom_map_elem_t modtcc_codecs_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_codecs)},
     {MP_ROM_QSTR(MP_QSTR_b58_encode), MP_ROM_PTR(&modtcc_b58_encode_obj)},
     {MP_ROM_QSTR(MP_QSTR_bech32_encode), MP_ROM_PTR(&modtcc_bech32_encode_obj)},
+    {MP_ROM_QSTR(MP_QSTR_bech32_plain_encode), MP_ROM_PTR(&modtcc_bech32_plain_encode_obj)},
+    {MP_ROM_QSTR(MP_QSTR_bech32_decode), MP_ROM_PTR(&modtcc_bech32_decode_obj)},
     {MP_ROM_QSTR(MP_QSTR_BECH32_ENCODING_BECH32), MP_ROM_INT(BECH32_ENCODING_BECH32)},
     {MP_ROM_QSTR(MP_QSTR_BECH32_ENCODING_BECH32M), MP_ROM_INT(BECH32_ENCODING_BECH32M)},
 };
