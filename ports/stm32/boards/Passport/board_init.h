68d6db9ee28aed4d3d9291b1e32d41599e519b81
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-03-07 15:30:29 +0100

    SFT-1462: Remove board_init.h file
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- ports/stm32/boards/Passport/board_init.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: © 2020 Foundation Devices, Inc. <hello@foundationdevices.com>
-// SPDX-License-Identifier: GPL-3.0-or-later
-//
-
-#ifndef __BOARD_INIT_H__
-#define __BOARD_INIT_H__
-
-#endif  // __BOARD_INIT_H__
