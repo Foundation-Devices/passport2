edfc6a30f4be853cac9c730ffdf02a1a2aba7769
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-03-10 15:05:43 +0100

    SFT-1513: Use ScanQRFlow in system camera test
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- ports/stm32/boards/Passport/modules/flows/system_test_camera_flow.py
+++ ports/stm32/boards/Passport/modules/flows/system_test_camera_flow.py
@@ -3,31 +3,20 @@
 #
 # system_test_camera_flow.py - Scans a QR code with the camera and shows the data in it
 
-from flows import Flow
-import microns
-from pages import ScanQRPage
-from pages import ErrorPage, SuccessPage, QuestionPage
+from flows import Flow, ScanQRFlow
+from pages import SuccessPage, QuestionPage
+from foundation import ur
 
 
 class TestCameraFlow(Flow):
-    def __init__(self, card_header=None, statusbar=None):
-        super().__init__(initial_state=self.scan_qr, name='TestQrScanFlow')
-        self.card_header = card_header
-        self.statusbar = statusbar
+    def __init__(self):
+        super().__init__(initial_state=self.scan_qr, name='TestCameraFlow')
 
     async def scan_qr(self):
-        result = await ScanQRPage(
-            card_header=self.card_header, statusbar=self.statusbar, right_micron=microns.Cancel).show()
+        result = await ScanQRFlow(data_description='a normal QR code').run()
         if result is None:
             skip = await QuestionPage(text='Skip the Camera test?').show()
             self.set_result(skip)
         else:
-            # Got a scan result (aka QRScanResult): good data or error
-            if result.is_failure():
-                # Unable to scan QR code - show error?
-                await ErrorPage(text='Unable to scan QR code.').show()
-                self.set_result(False)
-            else:
-                data = result.data
-                await SuccessPage(data).show()
-                self.set_result(True)
+            await SuccessPage(result).show()
+            self.set_result(True)
