2c9d63d0bdbc2f90e1b5fbb94f95df780ad1113a
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-03-15 13:27:35 +0100

    SFT-1494: Cleanup display header file
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- ports/stm32/boards/Passport/include/display.h
+++ ports/stm32/boards/Passport/include/display.h
@@ -36,21 +36,18 @@
 #define SPLASH_TEXT_BOTTOM_MARGIN 54
 
 extern void display_init(bool clear);
-extern void display_progress_bar(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t percent, char* message);
 extern void display_clear(uint16_t color);
 extern void display_fill(uint16_t color);
 extern void display_show(void);
 extern void display_clean_shutdown(void);
 
 #ifdef PASSPORT_BOOTLOADER
+extern void     display_progress_bar(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t percent, char* message);
 extern uint16_t display_measure_text(char* text, Font* font);
 extern uint16_t display_get_char_width(char ch, Font* font);
 extern void     display_text(char* text, int16_t x, int16_t y, Font* font, uint16_t color);
 extern void     display_fill_rect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
 extern void     display_rect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-extern void     display_glyph(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t* glyph, uint16_t color);
 extern void     display_image(
         uint16_t x, uint16_t y, uint16_t image_w, uint16_t image_h, const uint8_t* image, uint8_t mode);
-extern void display_repeat_image_horiz(
-    uint16_t x, uint16_t y, const lv_img_dsc_t* image, uint16_t repeat_count, uint8_t mode);
 #endif
