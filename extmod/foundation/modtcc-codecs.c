f8ffb37680c24d82cf17012f407a0a613186873d
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-03-16 17:57:03 +0100

    SFT-1570: Remove bech32_decode
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- extmod/foundation/modtcc-codecs.c
+++ extmod/foundation/modtcc-codecs.c
@@ -24,7 +24,6 @@
 #include "py/objstr.h"
 
 #include "base58.h"
-#include "hasher.h"
 #include "segwit_addr.h"
 
 //
@@ -146,73 +145,10 @@ STATIC mp_obj_t modtcc_bech32_encode(mp_obj_t hrp_obj, mp_obj_t segwit_version_o
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(modtcc_bech32_encode_obj, modtcc_bech32_encode);
 
-STATIC mp_obj_t modtcc_bech32_decode(mp_obj_t enc) {
-    const char* s = mp_obj_str_get_str(enc);
-
-    /** Decode a Bech32 string
- *
- *  Out: hrp:      Pointer to a buffer of size strlen(input) - 6. Will be
- *                 updated to contain the null-terminated human readable part.
- *       data:     Pointer to a buffer of size strlen(input) - 8 that will
- *                 hold the encoded 5-bit data values.
- *       data_len: Pointer to a size_t that will be updated to be the number
- *                 of entries in data.
- *  In: input:     Pointer to a null-terminated Bech32 string.
- *  Returns 1 if succesful.
-int bech32_decode(
-    char *hrp,
-    uint8_t *data,
-    size_t *data_len,
-    const char *input
-);
- */
-
-    char    hrp[strlen(s) + 16];
-    uint8_t tmp[strlen(s) + 16];  // actually 8-bit
-    size_t  tmp_len = 0;
-
-    int rv = bech32_decode(hrp, tmp, &tmp_len, s);
-
-    if (rv != 1) {
-        // probably transcription error from user
-        mp_raise_ValueError(MP_ERROR_TEXT("corrupt bech32"));
-    }
-
-    if (tmp_len <= 1) {
-        // lots of valid Bech32 strings, but invalid for segwit puposes
-        // can end up here; but don't care.
-        mp_raise_ValueError(MP_ERROR_TEXT("no sw verion and/or data"));
-    }
-
-    // re-pack 5-bit data into 8-bit bytes (after version)
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wvla-larger-than="
-    uint8_t packed[tmp_len];
-    #pragma GCC diagnostic pop
-    size_t  packed_len = 0;
-
-    int cv_ok = sw_convert_bits(packed, &packed_len, 8, tmp + 1, tmp_len - 1, 5, false);
-
-    if (cv_ok != 1) {
-        mp_raise_ValueError(MP_ERROR_TEXT("repack fail"));
-    }
-
-    // return a tuple: (hrp, version, data)
-    mp_obj_t tuple[3] = {
-        mp_obj_new_str(hrp, strlen(hrp)),
-        MP_OBJ_NEW_SMALL_INT(tmp[0]),
-        mp_obj_new_bytes(packed, packed_len),
-    };
-
-    return mp_obj_new_tuple(3, tuple);
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(modtcc_bech32_decode_obj, modtcc_bech32_decode);
-
 STATIC const mp_rom_map_elem_t modtcc_codecs_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_codecs)},
     {MP_ROM_QSTR(MP_QSTR_b58_encode), MP_ROM_PTR(&modtcc_b58_encode_obj)},
     {MP_ROM_QSTR(MP_QSTR_bech32_encode), MP_ROM_PTR(&modtcc_bech32_encode_obj)},
-    {MP_ROM_QSTR(MP_QSTR_bech32_decode), MP_ROM_PTR(&modtcc_bech32_decode_obj)},
 };
 STATIC MP_DEFINE_CONST_DICT(modtcc_codecs_globals, modtcc_codecs_globals_table);
 
