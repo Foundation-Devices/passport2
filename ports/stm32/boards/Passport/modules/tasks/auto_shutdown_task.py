21855ae6aef5c71d3ada80609b4d1d66b3ca5bcc
Matt Gleason <mjgleason@foundationdevices.com>
2023-03-17 14:48:32 -0500

    SFT-1585: made brightness restoration instant

--- ports/stm32/boards/Passport/modules/tasks/auto_shutdown_task.py
+++ ports/stm32/boards/Passport/modules/tasks/auto_shutdown_task.py
@@ -15,7 +15,6 @@ async def auto_shutdown_task():
         return
 
     common.last_interaction_time = utime.ticks_ms()
-    temp_brightness = False
 
     while True:
         await sleep_ms(1000)
@@ -23,11 +22,9 @@ async def auto_shutdown_task():
         # Very simple for now...just shutdown!
         timeout = common.settings.get('shutdown_timeout', 5 * 60)
         now = utime.ticks_ms()
+
         if timeout > 0 and now - common.last_interaction_time >= (timeout - 30) * 1000:
             common.display.set_brightness(2)
-            temp_brightness = True
-        elif temp_brightness:
-            common.display.set_brightness(get_screen_brightness(100))
-            temp_brightness = False
+
         if timeout > 0 and now - common.last_interaction_time >= timeout * 1000:
             common.system.shutdown()
