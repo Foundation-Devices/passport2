8a8825b0c0436338d8fabad4a572b574a508a26f
Matt Gleason <mjgleason@foundationdevices.com>
2023-03-19 13:04:18 -0500

    SFT-1557: created common flow for getting seed words

--- ports/stm32/boards/Passport/modules/flows/view_seed_words_flow.py
+++ ports/stm32/boards/Passport/modules/flows/view_seed_words_flow.py
@@ -3,13 +3,7 @@
 #
 # view_seed_words_flow.py - Confirm the user wants to see this sensitive info, then show it.
 
-import lvgl as lv
-import microns
-from pages import InfoPage, ErrorPage, LongTextPage, QuestionPage
 from flows import Flow
-from tasks import get_seed_words_task, get_words_from_seed_task
-from utils import spinner_task
-import stash
 
 
 class ViewSeedWordsFlow(Flow):
@@ -18,6 +12,10 @@ class ViewSeedWordsFlow(Flow):
         super().__init__(initial_state=self.show_intro, name='ViewSeedWordsFlow')
 
     async def show_intro(self):
+        import lvgl as lv
+        import microns
+        from pages import InfoPage
+
         text = 'Passport is about to display your seed words and, if defined, your passphrase.'
         result = await InfoPage(
             icon=lv.LARGE_ICON_SEED, text=text,
@@ -29,6 +27,8 @@ class ViewSeedWordsFlow(Flow):
             self.set_result(False)
 
     async def confirm_show(self):
+        from pages import QuestionPage
+
         result = await QuestionPage(
             'Anyone who knows these words can control your funds.\n\nDisplay this sensitive information?').show()
         if result:
@@ -37,17 +37,17 @@ class ViewSeedWordsFlow(Flow):
             self.set_result(False)
 
     async def show_seed_words(self):
-        if self.external_key:
-            (words, error) = await spinner_task(text='Retrieving Seed', task=get_words_from_seed_task,
-                                                args=[self.external_key])
-        else:
-            (words, passphrase, error) = await spinner_task('Retrieving Seed', get_seed_words_task)
-        if error is None and words is not None:
-            from pages import SeedWordsListPage
-            result = await SeedWordsListPage(words=words).show()
-            if stash.bip39_passphrase != '':
-                await InfoPage(text='Passphrase: {}'.format(stash.bip39_passphrase)).show()
-            self.set_result(result)
-        else:
-            await ErrorPage(text='Unable to retrieve seed: {}'.format(error)).show()
+        from flows import GetSeedWordsFlow
+        from pages import SeedWordsListPage, InfoPage
+        import stash
+
+        words = await GetSeedWordsFlow(self.external_key).run()
+
+        if words is None:
             self.set_result(False)
+            return
+
+        result = await SeedWordsListPage(words=words).show()
+        if stash.bip39_passphrase != '':
+            await InfoPage(text='Passphrase: {}'.format(stash.bip39_passphrase)).show()
+        self.set_result(result)
