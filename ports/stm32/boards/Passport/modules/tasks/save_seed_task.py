0e5687c0aba29cd275d59bc1f29f7008ed76b6e9
Matt Gleason <mjgleason@foundationdevices.com>
2023-04-05 13:21:10 -0500

    SFT-1618: handle errors in save_seed_task

--- ports/stm32/boards/Passport/modules/tasks/save_seed_task.py
+++ ports/stm32/boards/Passport/modules/tasks/save_seed_task.py
@@ -16,18 +16,21 @@ async def save_seed_task(on_done, seed_bits):
     from common import pa
     import stash
 
-    secret = SecretStash.encode(seed_bits=seed_bits)
+    try:
+        secret = SecretStash.encode(seed_bits=seed_bits)
 
-    pa.change(new_secret=secret)
+        pa.change(new_secret=secret)
 
-    # Recapture XFP, etc. for new secret
-    await pa.new_main_secret(secret)
+        # Recapture XFP, etc. for new secret
+        await pa.new_main_secret(secret)
 
-    # Check and reload secret
-    pa.reset()
-    pa.login()
+        # Check and reload secret
+        pa.reset()
+        pa.login()
 
-    with stash.SensitiveValues() as sv:
-        sv.capture_xpub()
+        with stash.SensitiveValues() as sv:
+            sv.capture_xpub()
+    except Exception as e:
+        await on_done(False)
 
     await on_done(None)
