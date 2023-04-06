6779ede70e84638f531a36f839d6a62784eb2554
Matt Gleason <mjgleason@foundationdevices.com>
2023-04-06 01:23:00 -0500

    SFT-1434: generated npub, waiting for delegation string

--- ports/stm32/boards/Passport/modules/tasks/nostr_key_task.py
+++ ports/stm32/boards/Passport/modules/tasks/nostr_key_task.py
@@ -7,10 +7,13 @@
 async def nostr_key_task(on_done, index):
     import stash
     import tcc
+    from trezorcrypto import secp256k1
 
     path = "m/44'/1237'/{}'/0/0".format(index)
     with stash.SensitiveValues() as sv:
         node = sv.derive_path(path)
         key = node.private_key()
-    key = tcc.codecs.bech32_plain_encode("nsec", key)
-    await on_done(key, None)
+        pub = secp256k1.publickey(key, True)[1:]
+    nsec = tcc.codecs.bech32_plain_encode("nsec", key)
+    npub = tcc.codecs.bech32_plain_encode("npub", pub)
+    await on_done({'priv': nsec, 'pub': npub}, None)
