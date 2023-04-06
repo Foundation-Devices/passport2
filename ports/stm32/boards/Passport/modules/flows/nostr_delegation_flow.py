b0019725466c62cb5d8e87dbda66caafc0808ba9
Matt Gleason <mjgleason@foundationdevices.com>
2023-04-06 13:44:10 -0500

    SFT-1434: first pass (untested) nip26 implementation, added timezones to support nip26

--- ports/stm32/boards/Passport/modules/flows/nostr_delegation_flow.py
+++ ports/stm32/boards/Passport/modules/flows/nostr_delegation_flow.py
@@ -6,13 +6,174 @@
 from flows import Flow
 
 
+def created_at_helper(carrot, created_at, timezone):
+    from datetime import datetime
+    created = [c for c in created_at if c.count(carrot)]
+    assert len(created) <= 1, 'There must only be 1 "created {}" condition.'.format(
+        'before' if carrot == '<' else 'after')
+
+    if len(created) == 0:
+        return None
+
+    created = int(created[0].split(carrot)[1])
+
+    # TODO: this could be a util
+    if timezone is not None:
+        datetime.timezone(datetime.timedelta(hours=int(timezone)))
+    created = datetime.fromtimestamp(created).strftime('%Y-%m-%d %H:%M')
+
+    # Ensure timezone string exists, prepend + if positive
+    # TODO: this could be a util
+    if timezone is None:
+        timezone = "+0"
+    elif int(timezone) >= 0:
+        timezone = "+" + timezone
+
+    return created + " GMT" + timezone
+
+
+event_kind = {
+    0: 'Metadata',
+    1: 'Short Text Notes',
+    2: 'Recommend Relays',
+    3: 'Contacts',
+    4: 'Encrypted Direct Messages',
+    5: 'Event Deletion',
+    6: 'Reposts',
+    7: 'Reactions',
+    8: 'Badge Awards',
+    40: 'Channel Creation',
+    41: 'Channel Metadata',
+    42: 'Channel Messages',
+    43: 'Channel Hide Messages',
+    44: 'Channel Mute Users',
+    1984: 'Reporting',
+    9731: 'Zap Requests',
+    9735: 'Zaps',
+    10000: 'Mute Lists',
+    10001: 'Pin Lists',
+    10002: 'Relay List Metadata',
+    24133: 'Nostr Connect',
+    30000: 'Categorized People Lists',
+    30001: 'Categorized Bookmark Lists',
+    30008: 'Profile Badges',
+    30009: 'Badge Definitions',
+    30023: 'Long-Form Content',
+    30078: 'Application-Specific Data',
+}
+
+
+def parse_delegation_string(delegation_string):
+    from ubinascii import unhexlify as a2b_hex
+    from utils import nostr_nip19_from_key
+    import uio
+    import re
+    from common import settings
+    from styles.colors import HIGHLIGHT_TEXT_HEX
+
+    fields = delegation_string.split(':')
+    num_fields = len(fields)
+    assert num_fields == 4, \
+        'Invalid delegation string: must have 4 fields, found {}'.format(num_fields)
+    assert field[0] == 'nostr' and field[1] == 'delegation', \
+        'First 2 fields of delegation string must be "nostr:delegation:"'
+
+    # exceptions handled by caller
+    delegatee = a2b_hex(field[2])
+
+    # TODO: check length before converting to npub
+
+    delegatee_npub = nostr_nip19_from_key(delegatee, "npub")
+
+    conditions = fields[3].split('&')
+    num_conditions = len(conditions)
+    assert num_conditions > 0, \
+        'Delegation conditions are required, found {}'.format(num_conditions)
+
+    # TODO: what are the condition parsing rules? Can multiple kinds be given different
+    # created_at rules within 1 delegation? I assume not for now. This means
+    # all 'kind' conditions must come before all 'created_at' conditions.
+
+    kind_pattern = re.compile('^kind=\\d+$')
+    created_at_pattern = re.compile('^created_at[\\<\\>]\\d+$')
+    kinds = []
+    created_at = []
+
+    # Get all kinds
+    for c in conditions:
+        if kind_pattern.search(c):
+            kinds.append(c)
+        else:
+            break
+
+    # Get all created_at
+    for c in conditions:
+        if created_at_pattern.search(c):
+            created_at.append(c)
+        else:
+            assert not kind_pattern.search(c), \
+                'All "kinds" must come before all "created_at" conditions.'
+            break
+
+    assert len(created_at) <= 2, 'There must be at most 2 "created_at" conditions.'
+
+    timezone = settings.get('timezone', None)
+    created_before = created_at_helper('<', created_at, timezone)
+    created_after = created_at_helper('>', created_at, timezone)
+
+    # len(kinds) + len(created_at) == len(conditions) iff all conditions are valid and
+    # all 'kinds' come before all 'created_at'
+    assert len(kinds) + len(created_at) == len(condtions), 'Invalid conditions found'
+
+    # temp = []
+    # for k in kinds:
+    #     temp.append(int(k.split('=')[1]))
+    # kinds = temp
+    kinds = list(map(lambda k: int(k.split('=')[1]), kinds))
+
+    # All inputs checked, format description and warnings
+
+    details = uio.StringIO()
+    details.write("\n{}\n{}".format(
+        recolor(HIGHLIGHT_TEXT_HEX, 'Delegating To:'),
+        delegatee_npub))
+
+    if created_after:
+        details.write("\n{}\n{}\n{}".format(
+            recolor(HIGHLIGHT_TEXT_HEX, 'Delegation Start Date:'),
+            created_after,
+            "Make sure this time isn't in the past!"))
+    else:
+        details.write("\n{}\n{}".format(
+            recolor(HIGHLIGHT_TEXT_HEX, 'Warning:'),
+            "No start date, use caution!"))
+
+    if created_before:
+        details.write("\n{}\n{}".format(
+            recolor(HIGHLIGHT_TEXT_HEX, 'Delegation End Date:'),
+            created_before))
+    else:
+        details.write("\n{}\n{}".format(
+            recolor(HIGHLIGHT_TEXT_HEX, 'Warning:'),
+            "No end date, use caution!"))
+
+    details.write("\n{}".format(
+        recolor(HIGHLIGHT_TEXT_HEX, 'Event Types:')))
+
+    for k in kinds:
+        details.write("\n{}: {}".format(
+            k, event_kind.get(k, "Unknown event, use caution!")))
+
+    return details.getvalue()
+
+
 class NostrDelegationFlow(Flow):
     def __init__(self, context=None):
         from derived_key import get_key_type_from_tn
 
         self.key = context
         self.key_type = get_key_type_from_tn(self.key['tn'])
-        self.nsec = None
+        self.pk = None
         self.npub = None
         self.delegation_string = None
         super().__init__(initial_state=self.export_npub, name='NostrDelegationFlow')
@@ -24,10 +185,10 @@ class NostrDelegationFlow(Flow):
         (vals, error) = await spinner_task(text='Generating npub',
                                                 task=self.key_type['task'],
                                                 args=[self.key['index']])
-        self.nsec = vals['priv']
-        self.npub = vals['pub']
+        self.pk = vals['pk']
+        self.npub = vals['npub']
 
-        if not self.nsec or not self.npub:
+        if not self.pk or not self.npub:
             await ErrorPage("Failed to generate npub and nsec").show()
             self.set_result(False)
             return
@@ -46,11 +207,53 @@ class NostrDelegationFlow(Flow):
             return
 
         if result.is_failure():
-            await ErrorPage(text='Unable to scan QR code.').show()
+            await ErrorPage('Unable to scan QR code.').show()
             self.set_result(False)
             return
 
         self.delegation_string = result.data
+        self.goto(self.display_details)
 
-        # TODO: parse delegation string, go to display info
+    async def display_details(self):
+        from pages import ErrorPage, InfoPage, LongTextPage
+        from common import settings
+        from flows import ChooseTimezoneFlow
+        import microns
+
+        if not settings.get('timezone', None):
+            result = await InfoPage('Nostr delegation is time-sensitive. Please enter your timezone.',
+                                    left_micron=microns.Back,
+                                    right_micron=microns.Forward).show()
+            if not result:
+                self.back()
+                return
+            await ChooseTimezoneFlow().run()
+
+        try:
+            details = parse_delegation_string(self.delegation_string)
+        except Exception as e:
+            await ErrorPage('{}'.format(e)).show()
+            self.set_result(False)
+            return
+
+        result = await LongTextPage(text=details,
+                                    centered=True,
+                                    card_header={'title': 'Delegation Details'}).show()
+        if not result:
+            self.set_result(False)
+            return
+
+        self.goto(self.sign_delegation)
+        return
+
+    async def sign_delegation(self):
+        from serializations import sha256
+        from utils import nostr_sign
+        from pages import ShowQRPage
+        from ubinascii import hexlify as b2a_hex
+
+        sha = sha256(self.delegation_string)
+        sig = nostr_sign(self.pk, sha)
+
+        await ShowQRPage(qr_data=b2a_hex(sig)).show()
         self.set_result(True)
--- ports/stm32/boards/Passport/modules/tasks/nostr_key_task.py
+++ ports/stm32/boards/Passport/modules/tasks/nostr_key_task.py
@@ -6,14 +6,18 @@
 
 async def nostr_key_task(on_done, index):
     import stash
-    import tcc
-    from trezorcrypto import secp256k1
+    # import tcc
+    # from trezorcrypto import secp256k1
+    from utils import nostr_pubkey_from_pk, nostr_nip19_from_key
 
     path = "m/44'/1237'/{}'/0/0".format(index)
     with stash.SensitiveValues() as sv:
         node = sv.derive_path(path)
         key = node.private_key()
-        pub = secp256k1.publickey(key, True)[1:]
-    nsec = tcc.codecs.bech32_plain_encode("nsec", key)
-    npub = tcc.codecs.bech32_plain_encode("npub", pub)
-    await on_done({'priv': nsec, 'pub': npub}, None)
+        # pub = secp256k1.publickey(key, True)[1:]
+    pub = nostr_pubkey_from_pk(key)
+    # nsec = tcc.codecs.bech32_plain_encode("nsec", key)
+    # npub = tcc.codecs.bech32_plain_encode("npub", pub)
+    nsec = nostr_nip19_from_key(key, "nsec")
+    npub = nostr_nip19_from_key(pub, "npub")
+    await on_done({'priv': nsec, 'npub': npub, 'pk': key, 'pub': pub}, None)
