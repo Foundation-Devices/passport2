30fc07b08570f1ebf5a8900a9cb56ad2cc354d45
Matt Gleason <mjgleason@foundationdevices.com>
2023-04-14 12:52:24 -0500

    SFT-1767: fixed syntax error, moved root_xfp check before capture_xpub checks

--- ports/stm32/boards/Passport/modules/ext_settings.py
+++ ports/stm32/boards/Passport/modules/ext_settings.py
@@ -211,9 +211,9 @@ class ExtSettings:
         else:
             # Special case for xfp and xpub -- make sure they exist and create if not
             if kn not in self.current:
+                if kn == 'root_xfp' and 'xfp' in self.current:
+                    return self.current.get('xfp', default)
                 if kn == 'xfp' or kn == 'xpub' or kn == 'root_xfp':
-                    kn == 'root_xfp' and 'xfp' in self.current:
-                        return self.current.get('xfp', default)
                     try:
                         # Update xpub/xfp in settings after creating new wallet
                         import stash
