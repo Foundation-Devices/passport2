79fc59f7cba70a9944f18fc0ee1191aaf99bb169
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-05-04 14:57:50 +0200

    SFT-1961: Move trezorcrypto flags to py.mk
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- ports/unix/mpconfigport.mk
+++ ports/unix/mpconfigport.mk
@@ -42,18 +42,11 @@ MICROPY_PY_JNI = 0
 # as submodules (currently affects only libffi).
 MICROPY_STANDALONE = 0
 
-CFLAGS_MOD += -DBITCOIN_ONLY=1 -DAES_128=1 -DAES_192=1
-
 MICROPY_EXTMOD_DIR = ../../extmod
 CFLAGS_MOD += -I$(MICROPY_EXTMOD_DIR) \
   -I$(MICROPY_EXTMOD_DIR)/foundation \
   -I$(MICROPY_EXTMOD_DIR)/quirc \
   -I$(MICROPY_EXTMOD_DIR)/uasyncio \
-  -I$(MICROPY_EXTMOD_DIR)/trezor-firmware/crypto \
-  -I$(MICROPY_EXTMOD_DIR)/trezor-firmware/crypto/aes \
-  -I$(MICROPY_EXTMOD_DIR)/trezor-firmware/crypto/chacha20poly1305 \
-  -I$(MICROPY_EXTMOD_DIR)/trezor-firmware/crypto/ed25519-donna \
-  -I$(MICROPY_EXTMOD_DIR)/trezor-firmware/core \
   -I$(MICROPY_EXTMOD_DIR)/../ports/stm32/boards/Passport/include \
   -I$(MICROPY_EXTMOD_DIR)/../ports/stm32/boards/Passport/common/micro-ecc
   # TODO: Move this the ports/stm32/boards/Passport include files?
