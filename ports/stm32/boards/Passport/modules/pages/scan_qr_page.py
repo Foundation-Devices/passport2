ddd3719404bb042b7b330b40e13ae12f23363a85
Matt Gleason <mjgleason@foundationdevices.com>
2023-06-01 13:02:23 -0500

    SFT-2105: blank microns have no actions for any ScanQRPage

--- ports/stm32/boards/Passport/modules/pages/scan_qr_page.py
+++ ports/stm32/boards/Passport/modules/pages/scan_qr_page.py
@@ -27,7 +27,6 @@ class ScanQRPage(Page):
                  statusbar=None,
                  left_micron=microns.Back,
                  right_micron=None,
-                 use_right_button=True,
                  qr_type=None,
                  max_frames=None):
         super().__init__(flex_flow=None,
@@ -40,7 +39,6 @@ class ScanQRPage(Page):
         self.prev_card_header = None
         self.timer = None
         self.camera = CameraQRScanner(qr_type)
-        self.use_right_button = use_right_button
         self.qr_type = qr_type
         self.max_frames = max_frames
 
@@ -128,12 +126,12 @@ class ScanQRPage(Page):
 
     # Just return None.
     def left_action(self, is_pressed):
-        if not is_pressed:
+        if not is_pressed and self.left_micron:
             self.set_result(None)
 
     # Either the user goes back or the scan finishes
     def right_action(self, is_pressed):
-        if not is_pressed and self.use_right_button:
+        if not is_pressed and self.right_micron:
             self.set_result(None)
 
 
