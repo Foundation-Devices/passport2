e2e12e4208da14077d13260b45c069def4611a38
Matt Gleason <mjgleason@foundationdevices.com>
2023-06-08 17:00:31 -0500

    SFT-2227: made some pages long for founders edition

---
+++ ports/stm32/boards/Passport/modules/pages/long_success_page.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: © 2023 Foundation Devices, Inc. <hello@foundationdevices.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# long_success_page.py
+
+import lvgl as lv
+from pages import LongTextPage
+from styles.colors import DEFAULT_LARGE_ICON_COLOR
+import microns
+
+
+class LongSuccessPage(LongTextPage):
+    def __init__(
+            self,
+            text=None,
+            card_header=None,
+            statusbar=None,
+            left_micron=None,
+            right_micron=microns.Checkmark):
+        super().__init__(
+            text=text,
+            card_header=card_header,
+            statusbar=statusbar,
+            icon=lv.LARGE_ICON_SUCCESS,
+            icon_color=DEFAULT_LARGE_ICON_COLOR,
+            centered=True,
+            left_micron=left_micron,
+            right_micron=right_micron)
