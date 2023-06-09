a00216fc378ded47bcf848605699b3a8c628dca0
Matt Gleason <mjgleason@foundationdevices.com>
2023-06-09 17:57:14 -0500

    SFT-2064: fixed bind function to account for async functions

--- ports/stm32/boards/Passport/modules/utils.py
+++ ports/stm32/boards/Passport/modules/utils.py
@@ -1395,12 +1395,16 @@ async def show_card_missing(flow):
         flow.set_result(result)
 
 
+# This assums the func passed in is async
 def bind(instance, func, as_name=None):
     if as_name is None:
         as_name = func.__name__
-    bound_method = func.__get__(instance, instance.__class__)
-    setattr(instance, as_name, bound_method)
-    return bound_method
+
+    async def method(*args, **kwargs):
+        return await func(instance, *args, **kwargs)
+
+    setattr(instance, as_name, method)
+    return method
 
 
 # EOF
