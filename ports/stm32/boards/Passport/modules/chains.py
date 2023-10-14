ac4a8bf9dfd16a7246f498c9d2fe976cea2a470e
Matt Gleason <mjgleason@foundationdevices.com>
2023-10-13 21:28:14 -0400

    SFT-1708: removed unused license

--- ports/stm32/boards/Passport/modules/chains.py
+++ ports/stm32/boards/Passport/modules/chains.py
@@ -4,9 +4,6 @@
 # SPDX-FileCopyrightText: 2018 Coinkite, Inc. <coldcardwallet.com>
 # SPDX-License-Identifier: GPL-3.0-only
 #
-# SPDX-FileCopyrightText: 2021 Emanuele Bellocchia
-# SPDX-License-Identifier: MIT
-#
 # (c) Copyright 2018 by Coinkite Inc. This file is part of Coldcard <coldcardwallet.com>
 # and is covered by GPLv3 license found in COPYING.
 #
