39e1fae043eb8927f134d1ae7cc709a05211e2b5
Matt Gleason <mjgleason@foundationdevices.com>
2023-10-17 03:14:03 -0400

    SFT-1707: added taproot export for envoy and reworked address type chooser page

--- ports/stm32/boards/Passport/modules/pages/address_type_chooser_page.py
+++ ports/stm32/boards/Passport/modules/pages/address_type_chooser_page.py
@@ -9,18 +9,25 @@ from public_constants import AF_P2WPKH, AF_P2WPKH_P2SH, AF_CLASSIC, AF_P2TR
 
 
 class AddressTypeChooserPage(ChooserPage):
-    OPTIONS = [
-        {'label': 'Segwit', 'value': AF_P2WPKH},
-        {'label': 'Wrapped Segwit', 'value': AF_P2WPKH_P2SH},
-        {'label': 'Legacy', 'value': AF_CLASSIC},
-        {'label': 'Taproot', 'value': AF_P2TR},
-    ]
+    LABELS = {
+        AF_P2WPKH: 'Segwit',
+        AF_P2WPKH_P2SH: 'Wrapped Segwit',
+        AF_CLASSIC: 'Legacy',
+        AF_P2TR: 'Taproot',
+    }
 
-    def __init__(self, card_header={'title': 'Address Type'}, initial_value=None,
-                 left_micron=None, right_micron=None):
+    def __init__(self, card_header={'title': 'Address Type'}, initial_value=None, options=None, left_micron=None, right_micron=None):
+
+        if options is None:
+            options = list(self.LABELS.keys())
+
+        final_options = []
+        for addr_type in options:
+            final_options.append({'label': self.LABELS[addr_type], 'value': addr_type})
+
         super().__init__(
             card_header=card_header,
-            options=self.OPTIONS,
-            initial_value=initial_value or self.OPTIONS[0].get('value'),
+            options=final_options,
+            initial_value=initial_value or final_options[0].get('value'),
             left_micron=left_micron,
             right_micron=right_micron)
