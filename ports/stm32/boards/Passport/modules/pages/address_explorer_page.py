f1c512e6afb9237c0e20d8e3d73826ac39a39491
Matt Gleason <mjgleason@foundationdevices.com>
2023-11-15 14:36:26 -0500

    SFT-967: fixed navigation freezing on address explorer

--- ports/stm32/boards/Passport/modules/pages/address_explorer_page.py
+++ ports/stm32/boards/Passport/modules/pages/address_explorer_page.py
@@ -27,12 +27,20 @@ class AddressExplorerPage(StatusPage):
 
     def attach(self, group):
         super().attach(group)
+        group.add_obj(self.lvgl_root)
         self.lvgl_root.add_event_cb(self.on_key, lv.EVENT.KEY, None)
         self.prev_top_level = common.ui.set_is_top_level(False)
+        # common.keypad.set_intercept_key_cb(self.on_key)
+        common.keypad.set_key_repeat(lv.KEY.UP, False)
+        common.keypad.set_key_repeat(lv.KEY.DOWN, False)
 
     def detach(self):
         common.ui.set_is_top_level(self.prev_top_level)
         self.lvgl_root.remove_event_cb(self.on_key)
+        lv.group_remove_obj(self.lvgl_root)
+        # common.keypad.set_intercept_key_cb(None)
+        common.keypad.set_key_repeat(lv.KEY.UP, True)
+        common.keypad.set_key_repeat(lv.KEY.DOWN, True)
         super().detach()
 
     def on_key(self, event):
