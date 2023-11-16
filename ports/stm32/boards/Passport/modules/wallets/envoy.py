c7c0e73b2e8750607b9189e211a033323c8efbed
Matt Gleason <mjgleason@foundationdevices.com>
2023-11-15 19:37:06 -0500

    SFT-1707: added taproot to envoy export, added microsd export option for debugging

--- ports/stm32/boards/Passport/modules/wallets/envoy.py
+++ ports/stm32/boards/Passport/modules/wallets/envoy.py
@@ -34,21 +34,7 @@ def create_envoy_export(sw_wallet=None,
     serial_num = system.get_serial_number()
     fw_version = 'v{}'.format(system.get_software_info()[0])
 
-    mode = get_bip_num_from_addr_type(addr_type, multisig)
-
     chain = chains.current_chain()
-
-    with stash.SensitiveValues() as sv:
-        acct_path = "m/{mode}'/{coin}'/{acct}'".format(
-            mode=mode,
-            coin=chain.b44_cointype,
-            acct=acct_num)
-
-        child_node = sv.derive_path(acct_path)
-        xfp = settings.get('xfp')
-        xpub = sv.chain.serialize_public(child_node, AF_CLASSIC)
-        # print('xfp to export: {}'.format(xfp))
-        # print('xpub to export: {}'.format(xpub))
 
     accounts = get_accounts()
     acct_name = ''
@@ -67,15 +53,32 @@ def create_envoy_export(sw_wallet=None,
 
     device_name = common.settings.get('device_name', '')
 
-    rv = dict(derivation=acct_path,
-              xfp=xfp,
-              xpub=xpub,
-              acct_name=acct_name,
+    rv = dict(acct_name=acct_name,
               acct_num=acct_num,
               hw_version=1.2 if passport.IS_COLOR else 1,
               fw_version=fw_version,
               serial=serial_num,
               device_name=device_name)
+
+    for name, mode in [
+        ('bip84', 84),
+        ('bip86', 86)
+    ]:
+        with stash.SensitiveValues() as sv:
+            acct_path = "m/{mode}'/{coin}'/{acct}'".format(
+                mode=mode,
+                coin=chain.b44_cointype,
+                acct=acct_num)
+
+            child_node = sv.derive_path(acct_path)
+            xfp = settings.get('xfp')
+            xpub = sv.chain.serialize_public(child_node, AF_CLASSIC)
+            # print('xfp to export: {}'.format(xfp))
+            # print('xpub to export: {}'.format(xpub))
+
+            rv[name] = dict(derivation=acct_path,
+                            xfp=xfp,
+                            xpub=xpub)
 
     # Return the possible account mappings that the exported wallet can choose from
     # When we get the address back, we can determine the 'fmt' from the address and then look it up to
@@ -83,8 +86,12 @@ def create_envoy_export(sw_wallet=None,
     accts = [{'fmt': addr_type, 'deriv': acct_path, 'acct': acct_num}]
 
     msg = ujson.dumps(rv)
+
+    if export_mode == 'qr':
+        return (ur.new_bytes(msg), accts)
     # print('msg={}'.format(to_str(msg)))
-    return (ur.new_bytes(msg), accts)
+
+    return (msg, accts)
 
 
 EnvoyWallet = {
@@ -93,10 +100,9 @@ EnvoyWallet = {
         {'id': 'single-sig', 'label': 'Single-sig', 'addr_type': AF_P2WPKH, 'create_wallet': create_envoy_export},
     ],
     'export_modes': [
-        {'id': 'qr', 'label': 'QR Code', 'qr_type': QRType.UR2}
+        {'id': 'qr', 'label': 'QR Code', 'qr_type': QRType.UR2},
+        {'id': 'microsd', 'label': 'microSD', 'filename_pattern': '{xfp}-envoy.json'}
     ],
     'skip_address_validation': False,
     'skip_multisig_import': True,
-    'select_addr_type': True,
-    'addr_options': [AF_P2WPKH, AF_P2TR],
 }
