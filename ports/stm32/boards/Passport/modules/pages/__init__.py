ef64560d0b65c57a357edaac6fc2a88364992ef8
Matt Gleason <mjgleason@foundationdevices.com>
2024-01-31 13:35:40 -0500

    SFT-3116: removed unused security words setting page

--- ports/stm32/boards/Passport/modules/pages/__init__.py
+++ ports/stm32/boards/Passport/modules/pages/__init__.py
@@ -39,7 +39,6 @@ from .scan_qr_page import *
 from .seed_words_list_page import *
 from .setup_mode_chooser_page import *
 from .shield_page import *
-from .show_security_words_setting_page import *
 from .show_qr_page import *
 from .shutdown_page import *
 from .singlesig_multisig_chooser_page import *
