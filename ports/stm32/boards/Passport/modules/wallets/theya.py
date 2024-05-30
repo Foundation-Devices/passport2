589e89123a22ebb54c7fdf23ec53dc52a77b7888
Matt Gleason <mjgleason@foundationdevices.com>
2024-05-30 18:20:37 -0400

    SFT-3670: added theya single-sig with firmware version info

--- ports/stm32/boards/Passport/modules/wallets/theya.py
+++ ports/stm32/boards/Passport/modules/wallets/theya.py
@@ -6,11 +6,13 @@
 
 from .multisig_import import read_multisig_config_from_qr, read_multisig_config_from_microsd
 from .multisig_json import create_multisig_json_wallet
+from .generic_json_wallet import create_generic_json_wallet
 from data_codecs.qr_type import QRType
 
 TheyaWallet = {
     'label': 'Theya',
     'sig_types': [
+        {'id': 'single-sig', 'label': 'Single-sig', 'addr_type': None, 'create_wallet': create_generic_json_wallet},
         {'id': 'multisig', 'label': 'Multisig', 'addr_type': None, 'create_wallet': create_multisig_json_wallet,
          'import_qr': read_multisig_config_from_qr, 'import_microsd': read_multisig_config_from_microsd}
     ],
