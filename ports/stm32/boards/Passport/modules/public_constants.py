57bc334b3ecbde03e0e9c8faa41cb890526444ed
Matt Gleason <mjgleason@foundationdevices.com>
2024-08-12 06:44:06 -0400

    SFT-3975: added security word toggle to device settings

--- ports/stm32/boards/Passport/modules/public_constants.py
+++ ports/stm32/boards/Passport/modules/public_constants.py
@@ -127,7 +127,8 @@ MARGIN_FOR_ADDRESSES = 0
 DEVICE_SETTINGS = [
     'screen_brightness',
     'shutdown_timeout',
-    'device_name'
+    'device_name',
+    'security_words'
 ]
 
 # Size of a pin prefix:
