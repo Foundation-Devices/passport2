398f23a685df10ba6ef23be888c99e3f1dbf4e55
qwldcl-del <qwldcl@users.noreply.github.com>
2026-03-15 05:20:33 +0800

    Add BIP-352 Silent Payment support
    - Add silent_payments.py module with functions for:
      - Decoding Silent Payment addresses (sp1.../tsp1...)
      - Encoding Silent Payment addresses
      - Address type detection
    
    - Update chains.py to import Silent Payment functions
    
    This implements basic Silent Payment address support as required
    for the bounty: Foundation-Devices/passport2#527

--- ports/stm32/boards/Passport/modules/chains.py
+++ ports/stm32/boards/Passport/modules/chains.py
@@ -17,6 +17,7 @@ from serializations import hash160, ser_compact_size
 from ucollections import namedtuple
 from opcodes import OP_CHECKMULTISIG
 from taproot import output_script
+from silent_payments import is_silent_payment_address, decode_silent_payment_address
 
 # See SLIP 132 <https://github.com/satoshilabs/slips/blob/master/slip-0132.md>
 # for background on these version bytes. Not to be confused with SLIP-32 which involves Bech32.
