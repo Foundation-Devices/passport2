66311050196fb3dd0c669b1f384171e10270dd6e
Jack <jack@foundation.xyz>
2026-07-23 14:55:00 +0200

    Add Bitcoin Safe wallet flow

---
+++ ports/stm32/boards/Passport/modules/wallets/bitcoin_safe.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2026 Foundation Devices, Inc. <hello@foundation.xyz>
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# bitcoin_safe.py - Bitcoin Safe wallet support
+#
+
+from .generic_json_wallet import create_generic_json_wallet
+from .multisig_json import create_multisig_json_wallet
+from .multisig_import import read_multisig_config_from_qr, read_multisig_config_from_microsd
+from data_codecs.qr_type import QRType
+
+BitcoinSafeWallet = {
+    'label': 'Bitcoin Safe',
+    'sig_types': [
+        {'id': 'single-sig', 'label': 'Single-sig', 'addr_type': None, 'create_wallet': create_generic_json_wallet},
+        {'id': 'multisig', 'label': 'Multisig', 'addr_type': None, 'create_wallet': create_multisig_json_wallet,
+         'import_qr': read_multisig_config_from_qr, 'import_microsd': read_multisig_config_from_microsd}
+    ],
+    'export_modes': [
+        {'id': 'qr', 'label': 'QR Code', 'qr_type': QRType.UR2},
+        {'id': 'microsd', 'label': 'microSD', 'filename_pattern': '{xfp}-bitcoin-safe.json',
+         'filename_pattern_multisig': '{xfp}-bitcoin-safe-multisig.json'}
+    ]
+}
