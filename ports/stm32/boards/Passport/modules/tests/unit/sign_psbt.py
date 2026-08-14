e1b9a88d51b250b40c7f5320e6967112159a7a54
Jack <jack@foundation.xyz>
2026-08-14 20:50:42 +0200

    Make OP_RETURN rendering tests palette independent

--- ports/stm32/boards/Passport/modules/tests/unit/sign_psbt.py
+++ ports/stm32/boards/Passport/modules/tests/unit/sign_psbt.py
@@ -1,9 +1,11 @@
-# SPDX-FileCopyrightText: 2026 Foundation Devices, Inc. <hello@foundation.xyz>
+# SPDX-FileCopyrightText: © 2026 Foundation Devices, Inc. <hello@foundation.xyz>
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 # Test trusted-display rendering for PSBT outputs.
 
 from flows.sign_psbt_common_flow import SignPsbtCommonFlow
+from styles.colors import HIGHLIGHT_TEXT_HEX
+from utils import escape_text, recolor, stylize_address
 
 
 class FakeChain:
@@ -16,6 +18,15 @@ class FakeChain:
 
 class FakeFlow:
     chain = FakeChain()
+
+
+class FakeAddressChain(FakeChain):
+    def render_address(self, script):
+        return script
+
+
+class FakeAddressFlow:
+    chain = FakeAddressChain()
 
 
 class FakeOutput:
@@ -39,10 +50,21 @@ def assert_op_return_output(value, message):
 assert_op_return_output(0, 'zero-value-message')
 assert_op_return_output(50000000, 'payment-id-12345')
 
-malicious_message = '#00bdcd Amount#\n0.00000001 BTC\n\n#00bdcd Destination#\nbc1qattacker'
+amount_heading = recolor(HIGHLIGHT_TEXT_HEX, 'Amount')
+message_heading = recolor(HIGHLIGHT_TEXT_HEX, 'Message')
+destination_heading = recolor(HIGHLIGHT_TEXT_HEX, 'Destination')
+malicious_message = '{}\n0.00000001 BTC\n\n{}\nbc1qattacker'.format(
+    amount_heading, destination_heading)
 rendered = SignPsbtCommonFlow.render_output(FakeFlow(), FakeOutput(1, malicious_message))
-assert rendered == (
-    '\n#00bdcd Amount#\n1 sats\n\n#00bdcd Message#\n'
-    '##00bdcd Amount##\n0.00000001 BTC\n\n##00bdcd Destination##\nbc1qattacker')
+assert escape_text(malicious_message) in rendered
+assert rendered.count('\n{}\n'.format(amount_heading)) == 1
+assert rendered.count('\n{}\n'.format(message_heading)) == 1
+assert rendered.count('\n{}\n'.format(destination_heading)) == 0
+assert malicious_message not in rendered
+
+address = 'bc1qvaliddestination'
+rendered = SignPsbtCommonFlow.render_output(FakeAddressFlow(), FakeOutput(42, address))
+assert rendered == '\n{}\n42 sats\n\n{}\n{}'.format(
+    amount_heading, destination_heading, stylize_address(address))
 
 return_value.write(b'OK')
