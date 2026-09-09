751d972ff6480079905d453fabe5ee785158e098
Matt Gleason <mjgleason@foundationdevices.com>
2026-09-09 13:53:08 -0400

    SFT-4502: handled further feedback

--- ports/stm32/boards/Passport/modules/tests/unit/psbt_change_validation.py
+++ ports/stm32/boards/Passport/modules/tests/unit/psbt_change_validation.py
@@ -8,10 +8,12 @@ from exceptions import FraudulentChangeOutput
 from psbt import psbtObject, psbtOutputProxy
 from serializations import CTxOut, hash160
 from taproot import output_script
+import trezorcrypto
 
 
 MY_XFP = 0x12345678
 PURPOSE_49 = 0x80000000 | 49
+PURPOSE_48 = 0x80000000 | 48
 PURPOSE_84 = 0x80000000 | 84
 PURPOSE_86 = 0x80000000 | 86
 COIN_0 = 0x80000000
@@ -19,13 +21,20 @@ ACCOUNT_0 = 0x80000000
 PUBKEY = b'\x02' + (b'\x11' * 32)
 TAP_PUBKEY = b'\x33' * 32
 PUBKEY_HASH = hash160(PUBKEY)
+MULTISIG_SCRIPT = b'\x51\x21' + PUBKEY + b'\x51\xae'
+MULTISIG_SCRIPT_HASH = trezorcrypto.sha256(MULTISIG_SCRIPT).digest()
 P2PK_SCRIPT = b'\x21' + PUBKEY + b'\xac'
 REDEEM_SCRIPT = b'\x00\x14' + PUBKEY_HASH
 GOOD_P2SH = b'\xa9\x14' + hash160(REDEEM_SCRIPT) + b'\x87'
 BAD_P2SH = b'\xa9\x14' + (b'\x22' * 20) + b'\x87'
 NATIVE_P2WPKH = b'\x00\x14' + PUBKEY_HASH
+LEGACY_P2SH = b'\xa9\x14' + hash160(MULTISIG_SCRIPT) + b'\x87'
+P2WSH_REDEEM_SCRIPT = b'\x00\x20' + MULTISIG_SCRIPT_HASH
+NESTED_P2WSH = b'\xa9\x14' + hash160(P2WSH_REDEEM_SCRIPT) + b'\x87'
+NATIVE_P2WSH = b'\x00\x20' + MULTISIG_SCRIPT_HASH
 TAPROOT_SCRIPT = output_script(TAP_PUBKEY, None)
 BIP49_SUBPATH = [MY_XFP, PURPOSE_49, COIN_0, ACCOUNT_0, 1, 7]
+BIP48_SUBPATH = [MY_XFP, PURPOSE_48, COIN_0, ACCOUNT_0, 0x80000000 | 2, 1, 7]
 BIP84_INPUT_SUBPATH = [MY_XFP, PURPOSE_84, COIN_0, ACCOUNT_0, 0, 3]
 BIP84_CHANGE_SUBPATH = [MY_XFP, PURPOSE_84, COIN_0, ACCOUNT_0, 1, 7]
 BIP86_INPUT_SUBPATH = [MY_XFP, PURPOSE_86, COIN_0, ACCOUNT_0, 0, 9]
@@ -37,11 +46,12 @@ BIP_UNKNOWN_SUBPATH = [MY_XFP, 0x80000000 | 123, COIN_0, ACCOUNT_0, 1, 7]
 class FakeOutput:
     validate = psbtOutputProxy.validate
 
-    def __init__(self, script_pubkey, subpaths=None, tap_subpaths=None, redeem_script=None):
+    def __init__(self, script_pubkey, subpaths=None, tap_subpaths=None, redeem_script=None,
+                 witness_script=None):
         self.subpaths = subpaths
         self.tap_subpaths = tap_subpaths
         self.redeem_script = redeem_script
-        self.witness_script = None
+        self.witness_script = witness_script
         self.is_change = False
         self._txo = CTxOut(0, script_pubkey)
 
@@ -90,6 +100,12 @@ class FakePsbt:
         self.warnings = []
 
 
+class OneOfOneMultisig:
+    def validate_script(self, script, subpaths):
+        assert script == MULTISIG_SCRIPT
+        assert subpaths == {PUBKEY: BIP48_SUBPATH}
+
+
 def assert_no_mixed_change_warning(outputs):
     mixed_inputs = [
         FakeInput(subpaths={PUBKEY: BIP84_INPUT_SUBPATH}, required_key=PUBKEY),
@@ -125,6 +141,20 @@ for path in (BIP84_SHORT_SUBPATH, BIP_UNKNOWN_SUBPATH):
     unsupported_path = FakeOutput(NATIVE_P2WPKH, subpaths={PUBKEY: path})
     unsupported_path.validate(0, unsupported_path._txo, MY_XFP, None)
     assert unsupported_path.is_change is False
+
+
+# BIP48 1-of-1 script wallets have one of our derivation entries, but still
+# require registered-wallet script validation rather than the single-sig path.
+for script_pubkey, redeem_script, witness_script in (
+        (LEGACY_P2SH, MULTISIG_SCRIPT, None),
+        (NESTED_P2WSH, P2WSH_REDEEM_SCRIPT, MULTISIG_SCRIPT),
+        (NATIVE_P2WSH, None, MULTISIG_SCRIPT)):
+    one_of_one_change = FakeOutput(script_pubkey,
+                                   subpaths={PUBKEY: BIP48_SUBPATH},
+                                   redeem_script=redeem_script,
+                                   witness_script=witness_script)
+    one_of_one_change.validate(0, one_of_one_change._txo, MY_XFP, OneOfOneMultisig())
+    assert one_of_one_change.is_change is True
 
 
 valid_mixed_segwit_change = FakeOutput(NATIVE_P2WPKH, subpaths={PUBKEY: BIP84_CHANGE_SUBPATH})
